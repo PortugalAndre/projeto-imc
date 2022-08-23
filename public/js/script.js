36bca2243e236bf00d2b5e364d7456423321259e
PortugalAndre <depaula_andrep@hotmail.com>
2022-08-23 19:48:46 -0300

    Esttilização da interface, JS recebendo inputs, inclusão da função de cálculo do IMC e o resultado é exibido no HTML.

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,27 @@
 function start() {
-  console.log('JS Ok');
-}
-start();
+  var inputPeso = document.querySelector('#inputPeso');
+  var inputAltura = document.querySelector('#inputAltura');
 
+  inputPeso.addEventListener('input', inputRecebido);
+  inputAltura.addEventListener('input', inputRecebido);
+}
+
+function calcularImc(peso, altura) {
+  //prettier-ignore
+  return peso / (altura ** 2);
+}
+
+//Função criada apenas para testar escutador de evento
+function inputRecebido() {
+  var peso = Number(inputPeso.value);
+  var altura = Number(inputAltura.value);
+  var imc = calcularImc(peso, altura);
+  resultadoImc.textContent = imc;
+}
+
+start();
+//incluir cálculo de IMC
 //IMC = Peso / (Altura ** 2)
+
+//Incluir sugestão de ingestão de água por dia
+//70(kg) X 0,035(L) = 2,45 L/dia
