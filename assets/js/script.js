16495bb183fbfe9e0925f3ac00994d7c7db193c8
André Portugal <depaula_andrep@hotmail.com>
2022-10-04 22:00:14 -0300

    correção de estrutura de seleção de faixa de IMC

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,13 +28,13 @@ function inputRecebido() {
   resultadoConsumoDeAgua.textContent = consumoDeAgua.toFixed(2) + 'L';
   if (imc < 18.5) {
     var faixaImc = 'Baixo peso';
-  } else if (imc >= 18.5 && imc <= 24.9) {
+  } else if (imc >= 18.5 && imc < 25) {
     var faixaImc = 'Estado nutricional adequado';
-  } else if (imc >= 25 && imc <= 29.9) {
+  } else if (imc >= 25 && imc < 30) {
     var faixaImc = 'Sobrepeso';
-  } else if (imc >= 30 && imc <= 34.9) {
+  } else if (imc >= 30 && imc < 35) {
     var faixaImc = 'Obesidade Grau I';
-  } else if (imc >= 35 && imc <= 39.9) {
+  } else if (imc >= 35 && imc < 40) {
     var faixaImc = 'Obesidade Grau II';
   } else if (imc >= 40) {
     var faixaImc = 'Obesidade Grau III';
